9457755f64dbc101131ba8e58f9a61f97a658437
1711020 <104405339+1711020@users.noreply.github.com>
2022-06-02 10:10:59 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+music.setVolume(127)
 basic.forever(function () {
-	
+    serial.writeLine("" + (sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )))
+    basic.showNumber(sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    ))
+})
+basic.forever(function () {
+    music.playMelody("C D A F C5 E G C ", 120)
+    music.playMelody("C D E F G A B C5 ", 120)
+    music.playMelody("G F A A G D E C ", 120)
 })
